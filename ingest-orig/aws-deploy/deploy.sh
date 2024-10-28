a2d2b020147411a9c9603d111779fea2cb1d20a3
liyajin7 <43145004+liyajin7@users.noreply.github.com>
2024-10-28 15:50:07 -0400

    trying reverted deploy.sh command: old version of aws, new version of docker

--- ingest-orig/aws-deploy/deploy.sh
+++ ingest-orig/aws-deploy/deploy.sh
@@ -12,17 +12,19 @@ mapfile -t current_repos <<< "$lines"
 # current_repos: name of the array variable that will store the lines of input
 # <<<: passes the content of a string to the command on the left side
 
+## Printing versions
+which docker
+docker --version
+aws --version
+
 ## New docker login for push
 # $(aws ecr get-login-password)
-#  printing docker versions
-which docker
-docker --version    
-aws --version
-aws ecr get-login-password | docker login --username AWS --password-stdin 021891577602.dkr.ecr.us-east-1.amazonaws.com
+
+# aws ecr get-login-password | docker login --username AWS --password-stdin 021891577602.dkr.ecr.us-east-1.amazonaws.com
 # docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 021891577602.dkr.ecr.us-east-1.amazonaws.com    
                                          #  ^^to avoid the TTY login error
 echo "DEBUG: docker login command ran"
-#$(aws ecr get-login --no-include-email) #  docker login for push
+$(aws ecr get-login --no-include-email) #  docker login for push
                                          #  deprection of the command get-login --no-include-email in awscli version 1.7.10
                                          #  aws ecr get-login-password | docker login --username AWS --password-stdin 1234567890.dkr.ecr.us-west-2.amazonaws.com/reponame
 
